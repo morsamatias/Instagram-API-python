e0dd14bce5873710de822ee8669e2c85a733e17d
Matias Morsa <33075003+morsamatias@users.noreply.github.com>
2019-03-03 16:42:09 -0300

    Update

--- examples/upload_photo.py
+++ examples/upload_photo.py
@@ -2,12 +2,19 @@
 # -*- coding: utf-8 -*-
 #
 # Use text editor to edit the script and type in valid Instagram username/password
+import time
 
 from InstagramAPI import InstagramAPI
 
 InstagramAPI = InstagramAPI("jnqov4", "MjYads9Yd22upWi")
 InstagramAPI.login()  # login
 
-photo_path = '0001.jpg'
+photo_path = '/dank/0001.jpg'
+caption = "#dank #dankmeme #dankest #dankmemesargentina #dankentina"
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+time.sleep(30)
+
+photo_path = '/dank/0002.jpg'
 caption = "#dank #dankmeme #dankest #dankmemesargentina #dankentina"
 InstagramAPI.uploadPhoto(photo_path, caption=caption)
